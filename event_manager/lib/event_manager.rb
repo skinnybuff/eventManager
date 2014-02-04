d4a942a416a39d265f05e96d634066bab00ca084
skinnybuff <s_t_white@yahoo.com>
2014-02-04 16:56:50 -0500

    csv lines are sorted and split into columns the first name is output from the array loop

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -2,5 +2,7 @@ puts "Event Manager Initialized!"
 
 lines = File.readlines "event_attendees.csv"
 lines.each do |line|
-	puts line
+	columns = line.split(",")
+	firstName = column[2]
+	puts firstName
 end
