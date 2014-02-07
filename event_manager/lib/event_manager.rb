87f16452dd5883ddfe34a48596dfe70b15ae3151
skinnybuff <s_t_white@yahoo.com>
2014-02-06 20:22:40 -0500

    fixed sysntax error on line 8

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -5,7 +5,7 @@ lines = File.readlines "event_attendees.csv"
 row_index = 0
 lines.each do |line|
 	row_index = row_index +1
-	next if == row_index == 1
+	next if row_index == 1
 	columns = line.split(",")
 	firstName = column[2]
 	puts firstName
