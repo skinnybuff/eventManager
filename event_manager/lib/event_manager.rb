37f206678062d983cfb289acfb956118fe42eb16
skinnybuff <s_t_white@yahoo.com>
2014-02-06 20:47:51 -0500

    made headers a symbol to reuse in hte loop

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,9 +1,9 @@
 require "csv"
 puts "Event Manager Initialized!"
 
-contents = csv.open "event_attendees.csv", headers: true
+contents = csv.open "event_attendees.csv", headers: true, header_converters: :symbol
 
 contents.each do |row|
-	name = row[2]
+	name = row[:first_name]
 	puts name
 end
