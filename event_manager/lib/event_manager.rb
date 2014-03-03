41f1b42a07f694b55ef11e232a7be989dff86798
skinnybuff <s_t_white@yahoo.com>
2014-03-03 18:06:22 -0500

    made symbols of the csv header items
    delt with epmty and short zipcode in the if else
    lines 10 -16
    
    look up syntax on line 10 (if zipcode.nil?) is this ternary
    zipcode == nil also works

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,9 +1,19 @@
-require "csv"
+require 'csv'
 puts "Event Manager Initialized!"
 
-contents = csv.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
 contents.each do |row|
 	name = row[:first_name]
-	puts name
+	zipcode = row[:zipcode]
+
+	if zipcode.nil?
+		zipcode = "00000"
+	elsif zipcode.length <5
+		zipcode = zipcode.rjust 5, "0"
+	elsif zipcode.length >5
+		zipcode = zipcode[0..4]
+	end
+
+	puts "#{name} #{zipcode}"
 end
