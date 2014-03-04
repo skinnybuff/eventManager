dfd9c0634d2747e0a5706df43c056183a3aa6629
skinnybuff <s_t_white@yahoo.com>
2014-03-04 14:53:24 -0500

    added else to the functino to catch the regular/ok zipcodes

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -3,10 +3,12 @@ require 'csv'
 def clean_zipcode(zipcode)
 	if zipcode.nil?
 		zipcode = "00000"
-	elsif zipcode.length <5
+	elsif zipcode.length < 5
 		zipcode = zipcode.rjust 5, "0"
-	elsif zipcode.length >5
+	elsif zipcode.length > 5
 		zipcode = zipcode[0..4]
+	else
+		zipcode
 	end
 end
 
@@ -16,7 +18,8 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 
 contents.each do |row|
 	name = row[:first_name]
-	zipcode = celean_zipcode(row[:zipcode])
+
+	zipcode = clean_zipcode(row[:zipcode])
 
 	puts "#{name} #{zipcode}"
 end
